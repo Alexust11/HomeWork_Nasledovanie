c3c7f94e1a1ad9ab1751cbdd029b0d990971ea21
Алексей <alexust11@yandex.ru>
2022-09-29 12:36:27 +0700

    урок наследование. Д/з 1. задание 1 и 2, каркас готов. Отсалось заполнить

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        Car bmw = new Car("bmw", "bmw X5", 2020, "FRG", "Black", 200, "автомат", 2.2f, "седан", "у477см124", 6, "летняя");
+        bmw.setRegNumber("q123iop124");
+        System.out.println(bmw.toString()+", модель- "+bmw.getBrand()+", марка- "+bmw.getModel()+", рег.номер- "+bmw.getRegNumber());
 
     }
 }
